a73f360041a0ae1b8fbc94921a0222398eff7534
emirhanaydin <emirhanaydin95@gmail.com>
2018-04-26 16:24:59 +0300

    Oyun ekranı temel görsel şablon ile oluşturuldu. Ana ekrandan oyun ekranına geçiş sağlandı.

--- app/src/main/java/com/example/kelimebulma/HomeActivity.java
+++ app/src/main/java/com/example/kelimebulma/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.kelimebulma;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -14,5 +17,13 @@ public class HomeActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textViewKullaniciAdi);
         String kullaniciAdi = getIntent().getStringExtra(getString(R.string.exta_kullanici_adi));
         textView.setText(kullaniciAdi);
+
+        Button playButton = findViewById(R.id.buttonOyna);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), GameActivity.class));
+            }
+        });
     }
 }
